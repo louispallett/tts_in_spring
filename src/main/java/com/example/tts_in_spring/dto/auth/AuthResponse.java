32f951f9a620f5dbc3d2cf3984584a0a2304a544
louispallett <louis.pallett@outlook.com>
2026-06-12 21:14:31 +0100

    refactor(Getters/Setters): Replace getters and setters with lombok annotations.

--- src/main/java/com/example/tts_in_spring/dto/auth/AuthResponse.java
+++ src/main/java/com/example/tts_in_spring/dto/auth/AuthResponse.java
@@ -1,14 +1,13 @@
 package com.example.tts_in_spring.dto.auth;
 
+import lombok.Getter;
+
+@Getter
 public class AuthResponse {
     private String token;
 
     public AuthResponse(String token) {
         this.token = token;
-    }
-
-    public String getToken() {
-        return token;
     }
 }
 
