2f687143b57b9aa9fdf6cfa20485a5e0582dd22b
louispallett <louis.pallett@outlook.com>
2026-06-15 19:21:23 +0100

    fix(dto): Add String stage to TournamentResponseLite.

--- src/main/java/com/example/tts_in_spring/dto/tournament/TournamentResponseLite.java
+++ src/main/java/com/example/tts_in_spring/dto/tournament/TournamentResponseLite.java
@@ -3,5 +3,6 @@ package com.example.tts_in_spring.dto.tournament;
 public record TournamentResponseLite (
         Long id,
         String name,
+        String stage,
         boolean showMobile
 ) {}
