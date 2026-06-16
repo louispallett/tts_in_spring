ce8bf5e8017479755889ee974c3cfe67e99f059f
louispallett <louis.pallett@outlook.com>
2026-06-16 08:48:33 +0100

    test(UserTestBuilder): Add password field.

--- src/test/java/com/example/tts_in_spring/user/UserTestBuilder.java
+++ src/test/java/com/example/tts_in_spring/user/UserTestBuilder.java
@@ -2,11 +2,12 @@ package com.example.tts_in_spring.user;
 
 public class UserTestBuilder {
     private Long id = 1L;
-    private final String firstName = "John";
-    private final String lastName = "Doe";
+    private String firstName = "John";
+    private String lastName = "Doe";
     private String email = "john.doe@example.com";
-    private final String mobCode = "+44";
-    private final String mobile = "1234567890";
+    private String mobCode = "+44";
+    private String mobile = "1234567890";
+    private String password = "Hello123!";
 
     public static UserTestBuilder aUser() {
         return new UserTestBuilder();
@@ -17,8 +18,23 @@ public class UserTestBuilder {
         return this;
     }
 
+    public UserTestBuilder withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public UserTestBuilder withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
     public UserTestBuilder withEmail(String email) {
         this.email = email;
+        return this;
+    }
+
+    public UserTestBuilder withPassword(String password) {
+        this.password = password;
         return this;
     }
 
@@ -30,6 +46,7 @@ public class UserTestBuilder {
         user.setEmail(email);
         user.setMobCode(mobCode);
         user.setMobile(mobile);
+        user.setPassword(password);
 
         return user;
     }
