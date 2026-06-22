7059f549b1a9e3eb0e2445c3fd41bdd78853fba0
louispallett <louis.pallett@outlook.com>
2026-06-22 13:28:30 +0100

    fix(TournamentService): Add missing findByCode method to repository.

--- src/main/java/com/example/tts_in_spring/tournament/TournamentRepository.java
+++ src/main/java/com/example/tts_in_spring/tournament/TournamentRepository.java
@@ -2,5 +2,8 @@ package com.example.tts_in_spring.tournament;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    Optional<Tournament> findByCode(String code);
 }
