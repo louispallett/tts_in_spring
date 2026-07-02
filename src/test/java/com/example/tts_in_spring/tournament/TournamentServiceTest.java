561ac51dc43da9b80a521d74ca40cac2b1c98438
louispallett <louis.pallett@outlook.com>
2026-07-02 10:04:50 +0100

    test(TournamentService): Add tests for validate method.

--- src/test/java/com/example/tts_in_spring/tournament/TournamentServiceTest.java
+++ src/test/java/com/example/tts_in_spring/tournament/TournamentServiceTest.java
@@ -155,6 +155,85 @@ public class TournamentServiceTest {
         verify(categoryService).createCategory(any(CategoryRequest.class), eq(currentUser.getId()));
     }
 
+    Category buildCategoryToValidate(int playerN, int maleN, boolean doubles, String name) {
+        Category category = CategoryTestBuilder.aCategory().build();
+        category.setName(name);
+        category.setDoubles(doubles);
+
+        for (long i = 1L; i <= maleN; i++) {
+            Player player = PlayerTestBuilder.aPlayer().withId(i).build();
+            category.getPlayers().add(player);
+        }
+
+        for (long i = maleN + 1; i <= playerN; i++) {
+            Player player = PlayerTestBuilder.aPlayer().withId(i).build();
+            player.setMale(false);
+            category.getPlayers().add(player);
+        }
+
+        return category;
+    }
+
+    @Test
+    void validate_whenValidAllCategories_returnsValidResponse() {
+        Tournament tournament = TournamentTestBuilder.aTournament().build();
+        Category maleSingles = buildCategoryToValidate(4, 4, false, "Mens Singles");
+        Category femaleSingles = buildCategoryToValidate(4, 0, false, "Womens Singles");
+        Category maleDoubles = buildCategoryToValidate(8, 8, true, "Mens Doubles");
+        Category femaleDoubles = buildCategoryToValidate(8, 0, true, "Womens Doubles");
+        Category mixDoubles = buildCategoryToValidate(16, 8, true, "Mixed Doubles");
+        tournament.setCategories(List.of(maleSingles, femaleSingles, maleDoubles, femaleDoubles, mixDoubles));
+
+        ValidateResponse response = new ValidateResponse(true, true, true, true);
+
+        when(tournamentFinder.getTournamentOrThrow(tournament.getId())).thenReturn(tournament);
+
+        assertThat(tournamentService.validate(tournament.getId(), tournament.getHost().getId())).isEqualTo(response);
+    }
+
+    @Test
+    void validate_whenInvalidAllCategories_returnsInvalidResponse() {
+        Tournament tournament = TournamentTestBuilder.aTournament().build();
+        Category maleSingles = buildCategoryToValidate(9, 9, false, "Mens Singles");
+        Category femaleSingles = buildCategoryToValidate(3, 0, false, "Womens Singles");
+        Category maleDoubles = buildCategoryToValidate(9, 9, true, "Mens Doubles");
+        Category femaleDoubles = buildCategoryToValidate(6, 0, true, "Womens Doubles");
+        Category mixDoubles = buildCategoryToValidate(18, 8, true, "Mixed Doubles");
+        tournament.setCategories(List.of(maleSingles, femaleSingles, maleDoubles, femaleDoubles, mixDoubles));
+
+        ValidateResponse response = new ValidateResponse(false, false, false, false);
+
+        when(tournamentFinder.getTournamentOrThrow(tournament.getId())).thenReturn(tournament);
+
+        assertThat(tournamentService.validate(tournament.getId(), tournament.getHost().getId())).isEqualTo(response);
+    }
+
+    @Test
+    void validate_whenValidSingleCategory_returnsValidResponse() {
+        Tournament tournament = TournamentTestBuilder.aTournament().build();
+        Category maleSingles = buildCategoryToValidate(9, 9, false, "Mens Singles");
+        tournament.getCategories().add(maleSingles);
+
+        ValidateResponse response = new ValidateResponse(true, true, true, true);
+
+        when(tournamentFinder.getTournamentOrThrow(tournament.getId())).thenReturn(tournament);
+
+        assertThat(tournamentService.validate(tournament.getId(), tournament.getHost().getId())).isEqualTo(response);
+    }
+
+    @Test
+    void validate_whenInvalidSingleCategory_returnsInvalidResponse() {
+        Tournament tournament = TournamentTestBuilder.aTournament().build();
+        Category mixDoubles = buildCategoryToValidate(18, 8, true, "Mixed Doubles");
+        tournament.getCategories().add(mixDoubles);
+
+        ValidateResponse response = new ValidateResponse(true, true, true, false);
+
+        when(tournamentFinder.getTournamentOrThrow(tournament.getId())).thenReturn(tournament);
+
+        assertThat(tournamentService.validate(tournament.getId(), tournament.getHost().getId())).isEqualTo(response);
+    }
+
      @Test
      void updateTournamentName_whenHost_savesAndReturnsMappedLite() {
          Tournament tournament = TournamentTestBuilder.aTournament().build();
