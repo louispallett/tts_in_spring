d7ffbe9fa1c8eb31f02edf9f2ee07ddc308fcd75
louispallett <louis.pallett@outlook.com>
2026-07-12 11:41:28 +0100

    fix(Emailer): Added @ConfigurationPropertiesScan to handle Bean injection for Resend configuration.

--- src/main/java/com/example/tts_in_spring/TtsInSpringApplication.java
+++ src/main/java/com/example/tts_in_spring/TtsInSpringApplication.java
@@ -2,8 +2,10 @@ package com.example.tts_in_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TtsInSpringApplication {
 
     public static void main(String[] args) {
