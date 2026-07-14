8905444c080841536e3a9802fc7026a9bac72c31
louispallett <louis.pallett@outlook.com>
2026-07-14 13:28:15 +0100

    test(DeleteUser): Update tests in accordance to 42fd14a.

--- src/test/java/com/example/tts_in_spring/auth/AuthServiceTest.java
+++ src/test/java/com/example/tts_in_spring/auth/AuthServiceTest.java
@@ -1,29 +1,20 @@
 package com.example.tts_in_spring.auth;
 
 import com.example.tts_in_spring.auth.dto.LoginRequest;
-import com.example.tts_in_spring.exception.UnauthorizedException;
 import com.example.tts_in_spring.user.User;
-import com.example.tts_in_spring.user.UserRepository;
+import com.example.tts_in_spring.user.UserFinder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
-    @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private BCryptPasswordEncoder passwordEncoder;
+    @Mock private UserFinder userFinder;
 
     @InjectMocks
     private AuthService authService;
@@ -39,37 +30,8 @@ public class AuthServiceTest {
         user.setEmail("john.doe@example.com");
         user.setPassword("hashed_password");
 
-        when(userRepository.findByEmail("john.doe@example.com")).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches("Hello123!", "hashed_password")).thenReturn(true);
+        when(userFinder.getUserByEmailWithPasswordOrThrow(request.email(), request.password())).thenReturn(user);
 
         assertThat(authService.login(request)).isEqualTo(1L);
-    }
-
-    @Test
-    void login_withInvalidPassword_throws401() {
-        LoginRequest request = new LoginRequest(
-                "john.doe@example.com",
-                "WrongPassword123!"
-        );
-        User user = new User();
-        user.setPassword("hashed_password");
-
-        when(userRepository.findByEmail("john.doe@example.com")).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches("WrongPassword123!", "hashed_password")).thenReturn(false);
-
-        assertThatThrownBy(() -> authService.login(request))
-                .isInstanceOf(UnauthorizedException.class);
-    }
-
-    @Test
-    void login_withUnknownEmail_throws401() {
-        LoginRequest request = new LoginRequest(
-                "random@random.com",
-                "AnyString123!"
-        );
-        when(userRepository.findByEmail("random@random.com")).thenReturn(Optional.empty());
-
-        assertThatThrownBy(() -> authService.login(request))
-                .isInstanceOf(UnauthorizedException.class);
     }
 }
--- src/test/java/com/example/tts_in_spring/user/UserFinderTest.java
+++ src/test/java/com/example/tts_in_spring/user/UserFinderTest.java
@@ -1,23 +1,30 @@
 package com.example.tts_in_spring.user;
 
+import com.example.tts_in_spring.auth.dto.LoginRequest;
 import com.example.tts_in_spring.exception.ResourceNotFoundException;
+import com.example.tts_in_spring.exception.UnauthorizedException;
+import com.example.tts_in_spring.user.dto.DeleteRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserFinderTest {
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserFinder userFinder;
@@ -37,5 +44,77 @@ public class UserFinderTest {
 
         assertThatThrownBy(() -> userFinder.getUserOrThrow(9L))
                 .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void getUserByEmailWithPasswordOrThrow_whenCorrectPassword_returnsUser() {
+        LoginRequest request = new LoginRequest(
+                "john.doe@example.com",
+                "Hello123!"
+        );
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@example.com");
+        user.setPassword("hashed_password");
+
+        when(userRepository.findByEmail(request.email())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.password(), user.getPassword())).thenReturn(true);
+
+        assertThat(userFinder.getUserByEmailWithPasswordOrThrow(request.email(), request.password())).isEqualTo(user);
+    }
+
+    @Test
+    void getUserByEmailWithPasswordOrThrow_whenIncorrectPassword_throws403() {
+        LoginRequest request = new LoginRequest(
+                "john.doe@example.com",
+                "Hello123!"
+        );
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@example.com");
+        user.setPassword("hashed_password");
+
+        when(userRepository.findByEmail(request.email())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.password(), user.getPassword())).thenReturn(false);
+
+        assertThatThrownBy(() -> userFinder.getUserByEmailWithPasswordOrThrow(request.email(), request.password()))
+                .isInstanceOf(UnauthorizedException.class);
+    }
+
+    @Test
+    void getUserByIdWithPasswordOrThrow_whenCorrectPassword_returnsUser() {
+        DeleteRequest request = new DeleteRequest(
+                "Hello123!"
+        );
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@example.com");
+        user.setPassword("hashed_password");
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.password(), user.getPassword())).thenReturn(true);
+
+        assertThat(userFinder.getUserByIdWithPasswordOrThrow(user.getId(), request.password())).isEqualTo(user);
+    }
+
+    @Test
+    void getUserByIdWithPasswordOrThrow_whenIncorrectPassword_throws403() {
+        DeleteRequest request = new DeleteRequest(
+                "Incorrect_password!"
+        );
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@example.com");
+        user.setPassword("hashed_password");
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.password(), user.getPassword())).thenReturn(false);
+
+        assertThatThrownBy(() -> userFinder.getUserByIdWithPasswordOrThrow(user.getId(), request.password()))
+                .isInstanceOf(UnauthorizedException.class);
     }
 }
